a2666e027d867df197c2b657f40b7d5e1f419cd9
andreydavi <andreydavi3ao@gmail.com>
2025-03-07 08:31:14 -0300

    Add files via upload

---
+++ js/exdois.js
@@ -0,0 +1,9 @@
+let nota1 = parseInt(Math.random()*11)
+let nota2 = parseInt(Math.random()*11)
+let nota3 = parseInt(Math.random()*11)
+let nota4 = parseInt(Math.random()*11)
+const media = nota1 + nota2 + nota3 + nota4/4;
+
+console.log(`Nota 1: ${nota1}, Nota 2: ${nota2}, Nota 3: ${nota3}, Nota 4: ${nota4} `);
+console.log(`Média: ${media}`);
+
